4eb1e19976c0187a2bc56abb554ae794d32551e5
Myller Silva <myllersilva1310@gmail.com>
2022-01-23 15:56:41 -0300

    update

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
+  const nome = "Myller";
+  const url = 'https://via.placeholder.com.150';
+
+
   return (
     <div className="App">
-      <h1>Ola mundo</h1>
-    
+      <h2>
+        alterando JSX
+      </h2>
+      <p>
+        Ola, {nome}
+      </p>
+      <img src={url} alt="Minha Imagem"/>
+      <HelloWorld></HelloWorld>
+
     </div>
   );
 }
---
+++ meu-projeto/src/components/HelloWorld.js
@@ -0,0 +1,15 @@
+import Frase from "./Frase";
+
+function HelloWorld() {
+    return(
+        <div>
+            <Frase></Frase>
+            <h1>
+                meu primeiro componente
+            </h1>
+            <Frase></Frase>
+        </div>
+    )
+}
+
+export default HelloWorld;
