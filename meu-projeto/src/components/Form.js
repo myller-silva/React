95ea1c49c8ef05e05688b8d303efc17ee381f8de
Myller Silva <myllersilva1310@gmail.com>
2022-01-27 21:41:25 -0300

    useState

--- meu-projeto/src/components/Form.js
+++ meu-projeto/src/components/Form.js
@@ -1,16 +1,39 @@
+import { useState } from 'react';
+
 function Form( ) {
     function cadastrarUsuario( event ) {
         // nao enviar o formulario pro servidor:
-        event.preventDefault();
-        console.log("usuario cadastrado");        
+        event.preventDefault();    
+        console.log(`Usuario: ${name}\nPassword: ${password}`);
     }
+
+    // name: nome do atributo que será lido/resgatado
+    // setName: o que será alterado
+    const [name, setName] = useState();
+    const [password, setPassword] = useState();
 
     return(
         <div>
             <h1>Meu cadastro</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="digite seu nome"/>
+                    <label htmlFor="name">Nome:</label>
+                    <input 
+                        type="text" 
+                        placeholder="digite seu nome" 
+                        id="name" name="name" 
+                        onChange={ (event) => setName(event.target.value)}
+                    />                    
+                </div>
+                <div>
+                    <label htmlFor="password">Senha:</label>
+                    <input 
+                        type="password" 
+                        id="password" 
+                        name="password" 
+                        placeholder="digite sua senha"
+                        onChange={ (event) => setPassword(event.target.value)}
+                    />
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar"/>
