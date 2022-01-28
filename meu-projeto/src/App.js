b63936ad9bc4a3c2c675febb1326ef40a78782a3
Myller Silva <myllersilva1310@gmail.com>
2022-01-28 12:13:29 -0300

    Renderização de listas

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,25 +1,15 @@
 import './App.css';
-// import HelloWorld from './components/HelloWorld';
-// import SayMyName from './components/SayMyName';
-// import Pessoa from './components/Pessoa';
-// import Frase from './components/Frase';
-// import List from './components/List';
-// import Evento from './components/evento/Evento';
-// import Form from './components/Form';
-import Condicional from './components/Condicional';
+import OutraLista from './components/OutraLista';
+
 function App() {
-  // const nome = "Myller";
-  // const nome2 = "Maria";
-  // const url = 'https://via.placeholder.com.150';
+  const meusItens = ["React", "Vue", "Angular"];
+
   return (
     <div className="App">
-      {/* <h1>Testando Eventos</h1>
-      <Evento/>
-      <Form/> */}
-
-      <h1>Renderizacao conficional</h1>
-      <Condicional></Condicional>
-      
+      <h1>Renderizacao de listas</h1>
+      <OutraLista itens={meusItens}/>
+      {/* lista vazia: */}
+      <OutraLista itens={[]}/>
     </div>
   );
 }
