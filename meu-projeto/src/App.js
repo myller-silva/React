3dac4bb3d93de35605395564f2beceffd685b1fc
Myller Silva <myllersilva1310@gmail.com>
2022-01-28 14:55:40 -0300

    React Router

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,17 +1,28 @@
-import './App.css';
-import { useState } from 'react';
-import SeuNome from './components/evento/SeuNome';
-import Saudacao from './components/Saudacao';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Empresa from './pages/Empresa';
+import Contato from './pages/Contato';
+import NavBarra from './components/layout/NavBarra';
+import Footer from './components/layout/Footer';
 
 function App() {
-  const [nome, setNome] = useState();
 
   return (
-    <div className="App">
-      <h1>State Lift</h1>
-      <SeuNome setNome={setNome}/>  
-      <Saudacao nome={nome}/>
-    </div>
+    <Router>
+      <NavBarra></NavBarra>
+      <Routes>
+        <Route exact path="/" element={<Home />} > </Route>
+      </Routes>
+      <Routes>
+        <Route path="/empresa" element={<Empresa />} > </Route>
+      </Routes>
+      <Routes>
+        <Route path="/contato" element={<Contato />} > </Route>
+      </Routes>
+
+      <Footer/>
+
+    </Router>
   );
 }
 
