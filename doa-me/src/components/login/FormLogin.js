f19534aac961c56f9e74af155dc56452520325b8
Myller Silva <myllersilva1310@gmail.com>
2022-01-30 21:35:41 -0300

    estilizando form de login

--- doa-me/src/components/login/FormLogin.js
+++ doa-me/src/components/login/FormLogin.js
@@ -1,16 +1,20 @@
+import style from './css/style.module.css';
+
+
+
 function FormLogin() {
     return(
-        <form >
-            <div>
+        <form className={style.FormLogin}>
+            <div className={style.InputsForm}>
                 <label>Email:</label>
                 <input type="email" placeholder="Digite seu email"></input>
             </div>
-            <div>
+            <div className={style.InputsForm}>
                 <label>Senha:</label>
                 <input type="password" placeholder="Digite sua senha"></input>
             </div>
-            <div>
-                <input type="submit" value="Login" id="btn-login"/>
+            <div className={style.btnLogin } >
+                <input type="submit" value="Login"/>
             </div>
         </form>
     )
--- doa-me/src/components/login/Login.js
+++ doa-me/src/components/login/Login.js
@@ -1,14 +1,18 @@
 import FormLogin from "./FormLogin";
-import style from './style.module.css'
+
+import style from './css/style.module.css';
 
 function Login() {
     return(
-        <div className={style.containerLogin}>
-            <div className={style.titulo}>
+        <div className={style.containerLogin} >
+            <div className={style.centralizar}>
                 <h2>Entrar</h2>
             </div>
-            <div className={style.containerLoginForm}>
+            <div className={style.centralizar }>
                 <FormLogin/>
+            </div>
+            <div className={style.centralizar}>
+                <a href="#">Esqueci minha senha</a>
             </div>
         </div>
     )
