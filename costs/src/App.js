b40ee8af7e7f8f4826c55dad57205a7a2387fe8f
Myller Silva <myllersilva1310@gmail.com>
2022-01-31 12:23:55 -0300

    footer

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,32 +1,32 @@
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
 import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Projects from './components/pages/Projects';
 
 
 function App() {
   return (
     <Router>
-      <div >
-        <Link to="/">Home</Link>
-        <Link to="/company">Companhia</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-      </div>
+      {/* barra de navegacao */}
+      <Navbar />
       
       <Container customClass="min_height">
         <Routes>
           <Route exact path='/' element={<Home/>}/>
+          <Route path='/projects' element={<Projects/>}/>
           <Route path='/company' element={<Company/>}/>
           <Route path='/contact' element={<Contact/>} />
           <Route path='/newproject' element={<NewProject/>} />
         </Routes>  
       </Container> 
 
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
---
+++ costs/src/components/pages/Projects.js
@@ -0,0 +1,8 @@
+// todos os projetos cadastrados
+
+function Projects() {
+    return <h1>Projetos cadastrados</h1>
+}
+
+
+export default Projects;
